9d2f938474e26409447eafc6826d7e97177c6f69
Procoder16 <smsoumik1608@gmail.com>
2022-07-24 23:55:18 +0530

    add proper indentation

--- Factorial_Algorithm.cpp
+++ Factorial_Algorithm.cpp
@@ -2,24 +2,22 @@
   
 using namespace std; 
      
-int main()    // main function: the execution of the program starts from here
-{    
-int factorial(int);  // function prototype
-int f,value;    
-cout<<"Enter any number: ";    
-cin>>value;    
-f=factorial(value);    
-cout<<"Factorial of "<<value<<" is "<<f<<endl;    
-return 0;    
+int main(){    // main function: the execution of the program starts from here    
+    int factorial(int);  // function prototype
+    int f,value;    
+    cout<<"Enter any number: ";    
+    cin>>value;    
+    f = factorial(value);    
+    cout<<"Factorial of "<<value<<" is "<<f<<endl;    
+    return 0;    
 }    
-int factorial(int n)  // funtion definition  
-{    
-if(n<0)    
-return(-1); /*Wrong value*/      
-if(n==0)    
-return(1);  /*Terminating condition*/    
-else    
-{    
-return(n*factorial(n-1));        
-}    
+int factorial(int n){  // funtion definition  
+    
+    if(n<0)    
+        return(-1); /*Wrong value*/      
+    if(n==0)    
+        return(1);  /*Terminating condition*/    
+    else{    
+        return(n*factorial(n-1));        
+    }    
 }  
